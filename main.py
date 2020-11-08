15ddc78780eea72eba52e929a75d53f0571b9c96
TheOWO <74142687+TheOWO@users.noreply.github.com>
2020-11-08 14:57:23 +0100

    Fixed bugs
    The bugs were fixed

--- main.py
+++ main.py
@@ -1,126 +1,126 @@
-from pip._vendor import requests
-from telegram.error import BadRequest
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-import owoify
-import json
-from pydub import AudioSegment
-import pylab
-import wave
-from zalgo_text import zalgo
+fwom pip._vendow impowt wequests
+fwom tewegwam.ewwow impowt BadWequest
+fwom tewegwam.ext impowt Updatew
+fwom tewegwam.ext impowt CommandHandwew
+impowt owoify
+impowt json
+fwom pydub impowt AudioSegment
+impowt pywab
+impowt wave
+fwom zawgo_text impowt zawgo
 
-updater = Updater(token='insert your bot token', use_context=True)
-dispatcher = updater.dispatcher
+updatew = Updatew(token='insewt youw bot token', use_context=Twue)
+dispatchew = updatew.dispatchew
 
 
 # Commands
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="*Hi! I'm running on transistors c:*\n\n"
+def stawt(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="*Hi! I'm wunning on twansistows c:*\n\n"
                                                                     "What can I do?\n"
-                                                                    "Type /help to see the available commands",
-                             parse_mode='markdown')
+                                                                    "Type /hewp to see the avaiwabwe commands",
+                             pawse_mode='mawkdown')
 
 
-def help(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="/trans - Documentation on transsexualism\n"
+def hewp(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="/twans - Documentation on twanssexuawism\n"
                                                                     "/owo - Wepwy to a message to OwOifi it\n"
-                                                                    "/zalgo - Reply to a message "
-                                                                    "to e̛͚n͔͜t́́e̽͜r̸͜ t̢͚h̰ͭẽ͝ V̵̱O̸̞I̡̟Ḏ́\n"
-                                                                    "/doggo - Send a random doggo\n"
-                                                                    "/spectral - Make a spectral analysis "
-                                                                    "of an audio file")
+                                                                    "/zawgo - Wepwy to a message "
+                                                                    "to e̛͚n͔͜t́́e̽͜w̸͜ t̢͚h̰ͭẽ͝ V̵̱O̸̞I̡̟Ḏ́\n"
+                                                                    "/doggo - Send a wandom doggo\n"
+                                                                    "/spectwaw - Make a spectwaw anyawysis "
+                                                                    "of an audio fiwe")
 
 
-def trans(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="https://bit.ly/34D6YDC")
+def twans(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="https://bit.wy/34D6YDC")
 
 
 def owo(update, context):
-    try:
-        message = update.message.reply_to_message.text
+    twy:
+        message = update.message.wepwy_to_message.text
         context.bot.send_message(chat_id=update.effective_chat.id, text=owoify.owoify(message),
-                                 reply_to_message_id=update.message.reply_to_message.message_id)
-    except AttributeError:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to a text message")
-    except TypeError:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to a text message")
+                                 wepwy_to_message_id=update.message.wepwy_to_message.message_id)
+    except AttwibuteEwwow:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Pwease wepwy to a text message")
+    except TypeEwwow:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Pwease wepwy to a text message")
 
 
-def zalgos(update, context):
-    try:
-        message = update.message.reply_to_message.text
-        context.bot.send_message(chat_id=update.effective_chat.id, text=zalgo.zalgo().zalgofy(message),
-                                 reply_to_message_id=update.message.reply_to_message.message_id)
-    except AttributeError:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to a text message")
-    except TypeError:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to a text message")
+def zawgos(update, context):
+    twy:
+        message = update.message.wepwy_to_message.text
+        context.bot.send_message(chat_id=update.effective_chat.id, text=zawgo.zawgo().zawgofy(message),
+                                 wepwy_to_message_id=update.message.wepwy_to_message.message_id)
+    except AttwibuteEwwow:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Pwease wepwy to a text message")
+    except TypeEwwow:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Pwease wepwy to a text message")
 
 
 def doggo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="(❍ᴥ❍ʋ)")
 
-    # retrieve image from source
-    f = r"https://random.dog/woof.json"
-    page = requests.get(f)
-    data = json.loads(page.text)
+    # wetwieve image fwom souwce
+    f = w"https://wandom.dog/woof.json"
+    page = wequests.get(f)
+    data = json.woads(page.text)
 
-    context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=data["url"])
+    context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=data["uww"])
 
 
-def spectral(update, context):
-    try:
-        message = update.message.reply_to_message.audio.get_file()
-        message.download("audio.file")
-        sound = AudioSegment.from_file("D:\Documents\Python\TransHandyBot/audio.file")
-        sound = sound.set_channels(1)
-        sound.export("audiomono.wav", format="wav")
+def spectwaw(update, context):
+    twy:
+        message = update.message.wepwy_to_message.audio.get_fiwe()
+        message.downwoad("audio.fiwe")
+        sound = AudioSegment.fwom_fiwe("D:\Documents\Python\TwansHandyBot/audio.fiwe")
+        sound = sound.set_channews(1)
+        sound.expowt("audiomono.wav", fowmat="wav")
 
-        def graph_spectrogram(wav_file):
-            sound_info, frame_rate = get_wav_info(wav_file)
-            pylab.figure(num=None, figsize=(10, 5), dpi=300)
-            pylab.subplot(111)
-            pylab.title('Spectral Analysis')
-            pylab.specgram(sound_info, Fs=frame_rate, cmap='inferno', scale_by_freq=get_wav_info(wav_file), NFFT=2048)
-            pylab.savefig('spectrogram.png', dpi=300)
+        def gwaph_spectwogwam(wav_fiwe):
+            sound_info, fwame_wate = get_wav_info(wav_fiwe)
+            pywab.figuwe(num=None, figsize=(10, 5), dpi=300)
+            pywab.subpwot(111)
+            pywab.titwe('Spectwaw Anyawysis')
+            pywab.specgwam(sound_info, Fs=fwame_wate, cmap='infewno', scawe_by_fweq=get_wav_info(wav_fiwe), NFFT=2048)
+            pywab.savefig('spectwogwam.png', dpi=300)
 
-        def get_wav_info(wav_file):
-            wav = wave.open(wav_file, 'r')
-            frames = wav.readframes(-1)
-            sound_info = pylab.fromstring(frames, 'int16')
-            frame_rate = wav.getframerate()
-            wav.close()
-            return sound_info, frame_rate
-    except BadRequest:
+        def get_wav_info(wav_fiwe):
+            wav = wave.open(wav_fiwe, 'w')
+            fwames = wav.weadfwames(-1)
+            sound_info = pywab.fwomstwing(fwames, 'int16')
+            fwame_wate = wav.getfwamewate()
+            wav.cwose()
+           wetuwn sound_info, fwame_wate
+    except BadWequest:
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="File is too big. Max size: 20MB",
-                                 reply_to_message_id=update.message.reply_to_message.message_id)
-    except AttributeError:
+                                 text="Fiwe is too big. Max size: 20MB",
+                                 wepwy_to_message_id=update.message.wepwy_to_message.message_id)
+    except AttwibuteEwwow:
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Please reply to a valid audio file")
+                                 text="Pwease wepwy to a vawid audio fiwe")
 
-    graph_spectrogram('audiomono.wav')
+    gwaph_spectwogwam('audiomono.wav')
     context.bot.sendPhoto(chat_id=update.effective_chat.id,
-                          reply_to_message_id=update.message.reply_to_message.message_id,
-                          photo=open('spectrogram.png', 'rb'))
+                          wepwy_to_message_id=update.message.wepwy_to_message.message_id,
+                          photo=open('spectwogwam.png', 'wb'))
 
 
-# cmd handlers
-start_handler = CommandHandler('start', start)
-help_handler = CommandHandler('help', help)
-trans_handler = CommandHandler('trans', trans)
-owo_handler = CommandHandler('owo', owo)
-doggo_handler = CommandHandler('doggo', doggo)
-spectral_handler = CommandHandler('spectral', spectral)
-zalgo_handler = CommandHandler('zalgo', zalgos)
+# cmd handwews
+stawt_handwew = CommandHandwew('stawt', stawt)
+hewp_handwew = CommandHandwew('hewp', hewp)
+twans_handwew = CommandHandwew('twans', twans)
+owo_handwew = CommandHandwew('owo', owo)
+doggo_handwew = CommandHandwew('doggo', doggo)
+spectwaw_handwew = CommandHandwew('spectwaw', spectwaw)
+zawgo_handwew = CommandHandwew('zawgo', zawgos)
 
-# cmd dispatchers
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(help_handler)
-dispatcher.add_handler(trans_handler)
-dispatcher.add_handler(owo_handler)
-dispatcher.add_handler(doggo_handler)
-dispatcher.add_handler(spectral_handler)
-dispatcher.add_handler(zalgo_handler)
+# cmd dispatchews
+dispatchew.add_handwew(stawt_handwew)
+dispatchew.add_handwew(hewp_handwew)
+dispatchew.add_handwew(twans_handwew)
+dispatchew.add_handwew(owo_handwew)
+dispatchew.add_handwew(doggo_handwew)
+dispatchew.add_handwew(spectwaw_handwew)
+dispatchew.add_handwew(zawgo_handwew)
 
-updater.start_polling()
+updatew.stawt_powwing()
